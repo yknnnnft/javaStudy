75696f8d910f8ff23317730f4c0902197725235f
li_re <li_re@worksap.co.jp>
2017-11-28 20:03:21 +0900

    refactorying main class to make it seen cleaner

--- src/selftest/CmdExecTester.java
+++ src/selftest/CmdExecTester.java
@@ -10,11 +10,15 @@ public class CmdExecTester {
 	private final String cmd_1 = "echo $PWD";
 	private final String cmd_2 = "cmd /c echo $PWD";
 
+	public void exec() {
+		testClassPath();
+	}
+
 	private String getCmd() {
 		return this.cmd_1;
 	}
 	
-	public void exec() {
+	private void testFlyway() {
 		ProcessBuilder pb = new ProcessBuilder("java", "-cp", "selftest.jar", "selftest.FlywayTester");
 		pb.redirectErrorStream(true);
 		Process p = null;
@@ -53,6 +57,49 @@ public class CmdExecTester {
 			} catch (IOException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
+			}
+		}
+	}
+	
+	private void testClassPath() {
+				ProcessBuilder pb = new ProcessBuilder("java", "-cp", "selftest.jar", "selftest.CmdExecTester");
+		Process p = null;
+		InputStream is = null;
+		BufferedReader br = null;
+		
+		pb.redirectErrorStream(true);
+		try {
+			p = pb.start();
+			is = p.getInputStream();
+			
+			p.waitFor();
+			
+			br = new BufferedReader(new InputStreamReader(is));
+			while (true) {
+				String line = br.readLine();
+				if (line == null) {
+					break;
+				}
+				System.out.println(line);
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			try {
+				
+				if (br != null) {
+					br.close();
+				}
+				if (is != null) {
+					is.close();
+				}
+			}
+			catch (IOException ie) {
+				ie.printStackTrace();
 			}
 		}
 	}
