2d446a356d2bac2178d21a3ba1b7ee6f01e44113
li_re <li_re@worksap.co.jp>
2017-12-01 21:06:35 +0900

    add test for org.apache.common.exec

--- src/selftest/CmdExecTester.java
+++ src/selftest/CmdExecTester.java
@@ -1,9 +1,20 @@
 package selftest;
 
 import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PipedInputStream;
+import java.io.PipedOutputStream;
+
+import org.apache.commons.exec.CommandLine;
+import org.apache.commons.exec.DefaultExecutor;
+import org.apache.commons.exec.ExecuteException;
+import org.apache.commons.exec.Executor;
+import org.apache.commons.exec.PumpStreamHandler;
 
 public class CmdExecTester {
 
@@ -11,7 +22,7 @@ public class CmdExecTester {
 	private final String cmd_2 = "cmd /c echo $PWD";
 
 	public void exec() {
-		testClassPath();
+		apacheExecutorTest();
 	}
 
 	private String getCmd() {
@@ -30,19 +41,19 @@ public class CmdExecTester {
 			System.out.println("test1");
 			p = pb.start();
 			System.out.println("test2");
-			p.waitFor();
-			int ret = p.exitValue();
-			System.out.println("ret: " + ret);
 			is = p.getInputStream();
 			br = new BufferedReader(new InputStreamReader(is));
 			
-			while(true) {
+			while(p.isAlive()) {
 				String line = br.readLine();
 				if (line == null) {
 					break;
 				}
 				System.out.println(line);
 			}
+			p.waitFor();
+			int ret = p.exitValue();
+			System.out.println("ret: " + ret);
 		} catch (IOException ie) {
 			ie.printStackTrace();
 			
@@ -62,7 +73,8 @@ public class CmdExecTester {
 	}
 	
 	private void testClassPath() {
-				ProcessBuilder pb = new ProcessBuilder("java", "-cp", "selftest.jar", "selftest.CmdExecTester");
+//		ProcessBuilder pb = new ProcessBuilder("java", "-cp", "selftest.jar", "selftest.CmdExecTester");
+		ProcessBuilder pb = new ProcessBuilder("ping", "192.168.182.213");
 		Process p = null;
 		InputStream is = null;
 		BufferedReader br = null;
@@ -72,16 +84,16 @@ public class CmdExecTester {
 			p = pb.start();
 			is = p.getInputStream();
 			
-			p.waitFor();
-			
 			br = new BufferedReader(new InputStreamReader(is));
+			int i = 0;
 			while (true) {
 				String line = br.readLine();
-				if (line == null) {
+				if (line == null && !p.isAlive()) {
 					break;
 				}
-				System.out.println(line);
+				System.out.println(++i + ": " + line);
 			}
+			p.waitFor();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -101,6 +113,52 @@ public class CmdExecTester {
 			catch (IOException ie) {
 				ie.printStackTrace();
 			}
+		}
+	}
+
+	public void apacheExecutorTest() {
+//		CommandLine cl = new CommandLine("cmd.exe");
+//		cl.addArgument("dir");
+//		cl.addArgument("c:\\");
+		CommandLine cl = new CommandLine("ping");
+		cl.addArgument("192.168.182.213");
+		try {
+			Executor e = new DefaultExecutor();
+			PipedOutputStream os = new PipedOutputStream();
+			PipedInputStream is = new PipedInputStream(os);
+			PumpStreamHandler psh = new PumpStreamHandler(os);
+			e.setStreamHandler(psh);
+			int ret;
+			Thread t = new Thread() {
+				@Override
+				public void run() {
+					System.out.println("Reading log...");
+					BufferedReader br = new BufferedReader(new InputStreamReader(is));
+					while (true) {
+						try {
+							String line = br.readLine();
+							if (line == null)
+								break;
+							System.out.println(line);
+						} catch (IOException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+
+					}
+					this.interrupt();
+				}
+			};
+			t.start();
+			ret = e.execute(cl);
+			while (t.isAlive()) { }
+			System.out.println(ret);
+		} catch (ExecuteException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
 		}
 	}
 
