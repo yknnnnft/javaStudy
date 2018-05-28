8dff1beb90f7986bfacd8dc824925020d8d33f27
li_re <li_re@worksap.co.jp>
2018-05-28 16:26:11 +0900

    fix for class initializing

--- src/selftest/ClassInitializingTester.java
+++ src/selftest/ClassInitializingTester.java
@@ -28,18 +28,18 @@ public class ClassInitializingTester {
 	}
 	
 	static public class Sub extends Parent{
-				
-		protected static String staticStr = "Static Variable in Sub";
-		protected String str = "Variable in Sub";
+		
+		protected static String staticStrInSub = "Static Variable in Sub";
+		protected String strInSub = "Variable in Sub";
 		
 		static {
-			System.out.println(staticStr);
+			System.out.println(staticStrInSub);
 			System.out.println("static initializing block in Sub");
 		}
 		
 		{
+			System.out.println(strInSub);
 			i = 3;
-			System.out.println(str);
 			System.out.println("i = " + i);
 			System.out.println("initializing block in Sub");
 		}
